7e8f6b2211be3a94171c058f7e0c1c44d6728acb
Erikvences <erikvences030@gmail.com>
2026-06-16 12:44:02 -0600

    Primer Commit

---
+++ src/main/java/com/utsem/app/TaqueriaApplication.java
@@ -0,0 +1,13 @@
+package com.utsem.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaqueriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaqueriaApplication.class, args);
+	}
+
+}
